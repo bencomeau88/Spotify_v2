13cda8a2f0ab0aef3c6909939b47fc656b630b9e
Benjamin Comeau <bcomeau@gwmail.gwu.edu>
2016-06-28 17:03:18 -0400

    need to GET multiple albums in JSON

--- app.js
+++ app.js
@@ -4,49 +4,67 @@ $(document).ready(function() {
     // this prevents default, submits the input.val() and...
     // clears fields
     $('.search_text').submit(function(e) {
-        var user_input = $('.artist').val();
-        var clear = function() {
-            $('.artist').val('');
-        }
-        e.preventDefault();
-
-    // $.ajax call to get artist searched for...
-    // in the above form field
-    var get_artist_data = function(user_input) {
-            $.ajax({
-                url: 'https://api.spotify.com/v1/search?	',
-                data: {
-                    type: 'artist',
-                    query: user_input
-                }
-            }).then(function(data) {
-            	console.log(data);
-                var object = data.artists.items[0];
-                var artist_id = object.id;
-                console.log(artist_id)
-                console.log(object)
-                for(i=0;i<object.images.length;i++){
-                	//how to add class to below
-                var album_covers = '<li>' +
-                '<img' + " src=" + object.images[i].url +'>' + 
-                '</li>'
-                $('.results').append(album_covers);
+            var user_input = $('.artist').val();
+            var clear = function() {
+                $('.artist').val('');
             }
-            })
-            //gets all the albums data...then move images to here
-            $.ajax({
-            	url:'https://api.spotify.com/v1/artists/'+ artist_id + 'albums',
-            	data: {
-            		type:'albums'
-            	}
-            }).then(function(data){
-            	console.log(data);
-            })
-        }
+            e.preventDefault();
 
-        // function calls
-    get_artist_data(user_input);
-    clear();
-    })
-    // end of code
+            // $.ajax call to get artist searched for...
+            // in the above form field
+            var get_artist_data = function(user_input) {
+                $.ajax({
+                    url: 'https://api.spotify.com/v1/search?	',
+                    data: {
+                        type: 'artist',
+                        query: user_input
+                    }
+                }).then(function(data) {
+                    console.log(data);
+                    var artist = data.artists.items[0];
+
+                    return artist;
+                }).then(function(artist) {
+                    console.log(artist);
+                    $.ajax({
+                        url: 'https://api.spotify.com/v1/artists/' + artist.id + '/albums',
+                        data: {
+                            type: 'albums'
+                        }
+                    }).then(function(data) {
+                    	var albums = data.items;
+                    	albums = _.sortBy(albums, '')
+                    	albums = _.uniq(albums,false,function(album){return album.name.slice(0,8).toLowerCase()})
+                        for (i = 0; i < albums.length; i++) {
+                        	var album = albums[i];
+                            //how to add class to below
+                            var album_covers = '<li>' +
+                                '<img' + " src=" + album.images[1].url + '>' +
+                                album.name +
+                                '</li>'
+                            $('.results').append(album_covers);
+                        }
+                        console.log(data);
+                       	// $.ajax({
+                       	// 	url:'https://api.spotify.com/v1/albums' +
+                       	// 	data:{
+                       	// 		type:'albums'
+                       	// 	}
+                       	// }).then(function(data){
+
+                       	// }
+                    })
+                })
+
+
+                //gets all the albums data...then move images to here
+
+
+            }
+
+            // function calls
+            get_artist_data(user_input);
+            clear();
+        })
+        // end of code
 })
