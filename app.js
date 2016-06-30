b8d6e03cc5e90683c2075173a01b39589b5e09a1
Benjamin Comeau <bcomeau@gwmail.gwu.edu>
2016-06-30 13:25:41 -0400

    got and appended albums, working on getting audio samples to work for preview

--- app.js
+++ app.js
@@ -1,70 +1,94 @@
 // beginning of code
 $(document).ready(function() {
-    // once someone enters the artist they are searching for...
-    // this prevents default, submits the input.val() and...
-    // clears fields
-    $('.search_text').submit(function(e) {
-            var user_input = $('.artist').val();
-            var clear = function() {
-                $('.artist').val('');
-            }
-            e.preventDefault();
+            // once someone enters the artist they are searching for...
+            // this prevents default, submits the input.val() and...
+            // clears fields
+            $('.search_text').submit(function(e) {
+                var user_input = $('.artist').val();
+                var clear = function() {
+                    $('.artist').val('');
+                }
+                e.preventDefault();
 
-            // $.ajax call to get artist searched for...
-            // in the above form field
-            var get_artist_data = function(user_input) {
-                $.ajax({
-                    url: 'https://api.spotify.com/v1/search?	',
-                    data: {
-                        type: 'artist',
-                        query: user_input
-                    }
-                }).then(function(data) {
-                    console.log(data);
-                    var artist = data.artists.items[0];
+                // $.ajax call to get artist searched for...
+                // in the above form field
+                var get_artist_data = function(user_input) {
+                        $.ajax({
+                            url: 'https://api.spotify.com/v1/search?	',
+                            data: {
+                                type: 'artist',
+                                query: user_input
+                            }
+                        }).then(function(data) {
+                            console.log(data);
+                            //returns the most popular aka first artist in the search list
+                            var artist = data.artists.items[0];
 
-                    return artist;
-                }).then(function(artist) {
-                    console.log(artist);
-                    $.ajax({
-                        url: 'https://api.spotify.com/v1/artists/' + artist.id + '/albums',
-                        data: {
-                            type: 'albums'
-                        }
-                    }).then(function(data) {
-                    	var albums = data.items;
-                    	albums = _.sortBy(albums, '')
-                    	albums = _.uniq(albums,false,function(album){return album.name.slice(0,8).toLowerCase()})
-                        for (i = 0; i < albums.length; i++) {
-                        	var album = albums[i];
-                            //how to add class to below
-                            var album_covers = '<li>' +
-                                '<img' + " src=" + album.images[1].url + '>' +
-                                album.name +
-                                '</li>'
-                            $('.results').append(album_covers);
-                        }
-                        console.log(data);
-                       	// $.ajax({
-                       	// 	url:'https://api.spotify.com/v1/albums' +
-                       	// 	data:{
-                       	// 		type:'albums'
-                       	// 	}
-                       	// }).then(function(data){
+                            return artist;
+                        }).then(function(artist) {
+                                console.log(artist);
+                                $.ajax({
+                                    url: 'https://api.spotify.com/v1/artists/' + artist.id + '/albums',
+                                    data: {
+                                        type: 'albums'
+                                    }
+                                }).then(function(data) {
+                                        var albums = data.items;
+                                        albums = _.sortBy(albums, '')
+                                        albums = _.uniq(albums, false, function(album) {
+                                            return album.name.slice(0, 8).toLowerCase() })
+                                        for (i = 0; i < albums.length; i++) {
+                                            var album = albums[i];
+                                            var album_id = albums[i].id;
+                                            console.log(album_id);
 
-                       	// }
-                    })
-                })
+                                            $.ajax({
+                                                url: 'https://api.spotify.com/v1/albums?' + album_id + ',',
+                                                data: {
+                                                    ids: album_id,
+                                                    'available_markets': 'ES'
+                                                }
+                                            }).then(function(data) {                            
+                                                    var preview_tracks = data.albums[0].tracks.items[0];
+                                                    console.log(preview_tracks);
+                                                    var album_images = data.albums[0].images[1];
+                                                    console.log(album_images);
+		                                            
+                                                    var album_covers = '<li>' +
+                                                    	'<a href="#">' +
+                                                        '<img' + " src=" + album_images.url + '>' +
+                                                        album.name +
+                                                        '</a>'+
+                                                        '</li>'
+                                                    $('.results').append(album_covers);
+                                                    var $album_info = $('a');
+                                                    $album_info.on('click',function(){
+                                                    	preview_audio = new Audio(preview_tracks.preview_url);                                                   
+                                                    	preview_audio.play();
+                                                    })
+                                                })
+
+                                            }
+                                        console.log(data);
+                                        // $.ajax({
+                                        // 	url:'https://api.spotify.com/v1/albums' +
+                                        // 	data:{
+                                        // 		type:'albums'
+                                        // 	}
+                                        // }).then(function(data){
+
+                                        // }
+                                    })
+                                })
 
 
-                //gets all the albums data...then move images to here
+                                //gets all the albums data...then move images to here
 
 
-            }
+                            }
 
-            // function calls
-            get_artist_data(user_input);
-            clear();
-        })
-        // end of code
-})
+                            // function calls
+                            get_artist_data(user_input); clear();
+                        })
+                    // end of code
+            })
