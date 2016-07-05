a8ebf7b56183f77c4a944dd5985aa0bbe82d4626
Benjamin Comeau <bcomeau@gwmail.gwu.edu>
2016-07-05 11:19:32 -0400

    js that successfully plays sample track from each album's first track

--- new.js
+++ new.js
@@ -19,7 +19,8 @@ $(document).ready(function() {
         });
 
         var get_albums = get_artist.then(function(data) {
-            var artist = data.artists.item[0];
+            console.log(data);
+            var artist = data.artists.items[0];
             console.log(artist);
             return $.ajax({
                 url: 'https://api.spotify.com/v1/artists/' + artist.id + '/albums',
@@ -44,15 +45,41 @@ $(document).ready(function() {
                 } else {
                     all_ids = all_ids + ',' + element.id
                 }
-                console.log(all_ids);
-                $.ajax({
-                    url: 'https://api.spotify.com/v1/albums',
-                    data: {
-                        ids: all_ids,
-                        markets: 'ES'
-                    }
-                }).then(console.log(data))
+
             })
+            console.log(all_ids);
+            return $.ajax({
+                url: 'https://api.spotify.com/v1/albums',
+                data: {
+                    ids: all_ids,
+                    markets: 'ES'
+                }
+            })
+
         });
+        get_album_info.then(function(data) {
+            data.albums.forEach(function(album, index) {
+                var album_cover = $('<li>' +
+                    '<a href="#">' +
+                    '<img' + " src=" + album.images[1].url + '>' +
+                    album.name +
+                    '</a>' +
+                    '</li>')
+                album_cover.on('click', function() {
+                	var preview_track = album.tracks.items[0].preview_url
+                    preview_audio = new Audio(preview_track);
+                    preview_audio.play();
+                })
+                $('.results').append(album_cover);
+            })
+            console.log(data);
+            var preview_track = data.albums[0].tracks.items[0].preview_url;
+            console.log(preview_track);
+            var album_images = data.albums[0].images[1];
+            console.log(album_images);
+            var $album_info = $('a');
+
+        })
+        
     });
 });
